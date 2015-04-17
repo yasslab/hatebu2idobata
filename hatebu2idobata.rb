dca908e50f7b36b4f81b156722a539dc5ed9a167
Yohei YASUKAWA <yasulab@gmail.com>
2015-04-17 17:04:26 +0900

    Update monitoring account: yasulabs -> yasslab

--- hatebu2idobata.rb
+++ hatebu2idobata.rb
@@ -6,14 +6,16 @@ require 'idobata'
 
 Idobata.hook_url = ENV['IDOBATA_END']
 HATEBU_USERS = [
-  "yasulabs",
-  "h6n"
+  #"yasulabs",
+  "yasslab",
+  #"h6n"
 ]
 
 msg = ""
 HATEBU_USERS.each { |user|
   # Flush cache RSS before downloading
-  `curl -H 'Pragma: no-cache' -L b.hatena.ne.jp/yasulabs/rss`
+  #`curl -H 'Pragma: no-cache' -L b.hatena.ne.jp/yasulabs/rss`
+  `curl -H 'Pragma: no-cache' -L b.hatena.ne.jp/yasslab/rss`
 
   rss = RSS::Parser.parse("http://b.hatena.ne.jp/#{user}/rss")
 
@@ -23,7 +25,7 @@ HATEBU_USERS.each { |user|
   end
 
   msg << bookmarks.map {|b|
-    p "<a href='#{b.link}'>#{b.title}</a> by <span class='label'>#{b.dc_creator}</span><br /> <b>#{b.description}<b/>"
+    p "<a href='#{b.link}'>#{b.title}</a> by <span class='label label-info'>#{user}</span><br /> <b>#{b.description}<b/>"
   }.join("<br/>")
 }
 
